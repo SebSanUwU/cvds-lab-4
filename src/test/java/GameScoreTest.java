d8ccd47fd2a7b1a19644053fba6aee542426859a
SebSanUwU <juancamargo206@gmail.com>
2023-09-10 14:36:26 -0500

    implementación pruebas

---
+++ src/test/java/GameScoreTest.java
@@ -0,0 +1,55 @@
+import hangman.model.*;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class GameScoreTest {
+    @Test
+    public void shouldOriginalScoreSum(){
+
+    }
+
+    @Test
+    public void shouldOriginalScoreSubtract(){
+
+    }
+
+    @Test
+    public void shouldOriginalScoreNotBelowZero(){
+
+    }
+
+    @Test
+    public void shouldBonusScoreSum(){
+
+    }
+
+    @Test
+    public void shouldBonusScoreSubtract(){
+
+    }
+    @Test
+    public void shouldBonusScoreNotBelowZero(){
+
+    }
+
+    @Test
+    public void shouldPowerBonusScoreSum(){
+
+    }
+
+    @Test
+    public void shouldPowerBonusScoreBehaviorPowerBe(){
+
+    }
+
+    @Test
+    public void shouldPowerBonusScoreSubtract(){
+
+    }
+    @Test
+    public void shouldPowerBonusScoreNotBelowZero(){
+
+    }
+
+}
